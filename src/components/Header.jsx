9c9cce4b1f78a24dfadf4336878cd95d94320ff5
Kali-Venum <subhamdas.abcd@gmail.com>
2023-06-29 00:00:31 +0530

    nav bar responsive.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 function Header() {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <div className='bg-[#2699fb] p-4'>
-      <div className='flex items-center py-[15px] justify-between max-w-[1240px] mx-auto'>
-        <div className='text-3xl font-bold'>
-          Nav Bar
-        </div>
-        <ul className='hidden md:flex text-white gap-10'>
-          <li>
-            Home
-          </li>
-          <li>
-            Company
-          </li>
-          <li>
-            Resources
-          </li>
-          <li>
-            About
-          </li>
-          <li>
-            Contact
-          </li>
+    <div className="bg-[#2699fb] p-4">
+      <div className="flex items-center py-[15px] justify-between max-w-[1240px] mx-auto">
+        <div className="text-3xl font-bold">Nav Bar</div>
+
+        {toggle ? (
+          <AiOutlineClose
+            onClick={() => setToggle((state) => !state)}
+            className="text-white text-2xl md:hidden block"
+          />
+        ) : (
+          <AiOutlineMenu
+            onClick={() => setToggle((state) => !state)}
+            className="text-white text-2xl md:hidden block"
+          />
+        )}
+
+        <ul className="hidden md:flex text-white gap-10">
+          <li>Home</li>
+          <li>Company</li>
+          <li>Resources</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        {/* Responsive Menu */}
+        <ul
+          className={`duration-300 md:hidden w-full h-screen text-white fixed bg-black top-[97px] ${
+            toggle ? "left-[0%]" : "left-[-100%]"
+          }`}
+        >
+          <li className="p-4">Home</li>
+          <li className="p-4">Company</li>
+          <li className="p-4">Resources</li>
+          <li className="p-4">About</li>
+          <li className="p-4">Contact</li>
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
