b81eab4cc59a1f50646c57055d79ace412ae1869
weaverswebsubham <subham.das@weavers-web.com>
2023-08-09 14:45:54 +0530

    working on the laptop secrion.

--- src/components/Experts.jsx
+++ src/components/Experts.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
+// Images.
+import laptopImage from "../assets/images/laptop.jpg"
+import Button from './Button'
+
 function Experts() {
   return (
-    <div className='max-w-[1240px] mx-auto my-10 h-[200px] border border-black grid grid-cols-3'>
-        <div className='border border-[blue] h-[200px] col-span-1'></div>
-        <div className='border border-[blue] h-[200px] col-span-2'></div>
+    <div className='max-w-[1240px] mx-auto my-10 grid grid-cols-2'>
+        <div className='col-span-1 w-[80%]'>
+          <img src={laptopImage} alt='laptop image' />
+        </div>
+        <div className='col-span-1 flex flex-col justify-center'>
+          <h1 className='text-[#00df9a] font-bold my-2'>LEARN FROM EXPERTS</h1>
+          <p className='my-2'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe totam modi eos culpa consequatur quod praesentium sit, non inventore atque ut reiciendis amet ullam earum exercitationem nemo iure esse voluptate.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe totam modi eos culpa consequatur quod praesentium sit, non inventore atque ut reiciendis amet ullam earum exercitationem nemo iure esse voluptate.
+          </p>
+          <Button styles={"bg-black text-white p-3 rounded w-[30%]"}>Get Started</Button>
+        </div>
     </div>
   )
 }
